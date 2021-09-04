6eaf3c094c8dfca3d13b65013e7d7eedb5c7c3ad
Alap Raval <alapsraval@gmail.com>
2021-09-03 22:35:14 -0500

    moved images to public folder

---
+++ src/data/projects.js
@@ -0,0 +1,54 @@
+export const projects = [
+    {
+        title: "Workout Tracker using MongoDB",
+        image: "./assets/img/workout-tracker.png",
+        web: "https://alap-workout-tracker.herokuapp.com/",
+        github: "https://github.com/alapsraval/workout-tracker"
+    },
+    {
+        title: "Data Analysis using Pandas",
+        image: "./assets/img/project-1.jpeg",
+        github: "https://github.com/alapsraval/pandas-data-analysis"
+    },
+    {
+        title: "Plotly Interactive Dashboard",
+        image: "./assets/img/project-2.jpeg",
+        web: "https://alapsraval.github.io/plotly-interactive-dashboard/",
+        github: "https://github.com/alapsraval/plotly-interactive-dashboard"
+    },
+    {
+        title: "Covid Catcher",
+        image: "./assets/img/coronavirus.jpeg",
+        web: "https://alapsraval.github.io/covid-catcher/",
+        github: "https://github.com/alapsraval/covid-catcher"
+    },
+    {
+        title: "Weather Dashboard",
+        image: "./assets/img/weather-dashboard.jpeg",
+        web: "https://alapsraval.github.io/weather-dashboard/",
+        github: "https://github.com/alapsraval/weather-dashboard"
+    },
+    {
+        title: "Coding Quiz",
+        image: "./assets/img/html.jpeg",
+        web: "https://alapsraval.github.io/html-coding-quiz/",
+        github: "https://github.com/alapsraval/html-coding-quiz"
+    },
+    {
+        title: "Password Generator",
+        image: "./assets/img/password.jpeg",
+        web: "https://alapsraval.github.io/password-generator/",
+        github: "https://github.com/alapsraval/password-generator"
+    },
+    {
+        title: "Web Scraping with Python",
+        image: "./assets/img/project-4.jpeg",
+        github: "ttps://github.com/alapsraval/web-scraping-application"
+    },
+    {
+        title: "Data Journalism and D3",
+        image: "./assets/img/project-5.jpeg",
+        web: "https://alapsraval.github.io/D3-data-journalism/",
+        github: "https://github.com/alapsraval/D3-data-journalism/"
+    },
+];
