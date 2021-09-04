646547a34cea0274410ef119e33a71f7269c0ed4
Alap Raval <alapsraval@gmail.com>
2021-09-04 15:22:57 -0500

    added compressed images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,53 +1,53 @@
 export const projects = [
     {
         title: "Workout Tracker using MongoDB",
-        image: "./assets/img/workout-tracker.png",
+        image: "./assets/img/workout-tracker-min.png",
         web: "https://alap-workout-tracker.herokuapp.com/",
         github: "https://github.com/alapsraval/workout-tracker"
     },
     {
         title: "Data Analysis using Pandas",
-        image: "./assets/img/project-1.jpeg",
+        image: "./assets/img/project-1-min.jpeg",
         github: "https://github.com/alapsraval/pandas-data-analysis"
     },
     {
         title: "Plotly Interactive Dashboard",
-        image: "./assets/img/project-2.jpeg",
+        image: "./assets/img/project-2-min.jpeg",
         web: "https://alapsraval.github.io/plotly-interactive-dashboard/",
         github: "https://github.com/alapsraval/plotly-interactive-dashboard"
     },
     {
         title: "Covid Catcher",
-        image: "./assets/img/coronavirus.jpeg",
+        image: "./assets/img/coronavirus-min.jpeg",
         web: "https://alapsraval.github.io/covid-catcher/",
         github: "https://github.com/alapsraval/covid-catcher"
     },
     {
         title: "Weather Dashboard",
-        image: "./assets/img/weather-dashboard.jpeg",
+        image: "./assets/img/weather-dashboard-min.jpeg",
         web: "https://alapsraval.github.io/weather-dashboard/",
         github: "https://github.com/alapsraval/weather-dashboard"
     },
     {
         title: "Coding Quiz",
-        image: "./assets/img/html.jpeg",
+        image: "./assets/img/html-min.jpeg",
         web: "https://alapsraval.github.io/html-coding-quiz/",
         github: "https://github.com/alapsraval/html-coding-quiz"
     },
     {
         title: "Password Generator",
-        image: "./assets/img/password.jpeg",
+        image: "./assets/img/password-min.jpeg",
         web: "https://alapsraval.github.io/password-generator/",
         github: "https://github.com/alapsraval/password-generator"
     },
     {
         title: "Web Scraping with Python",
-        image: "./assets/img/project-4.jpeg",
+        image: "./assets/img/project-4-min.jpeg",
         github: "ttps://github.com/alapsraval/web-scraping-application"
     },
     {
         title: "Data Journalism and D3",
-        image: "./assets/img/project-5.jpeg",
+        image: "./assets/img/project-5-min.jpeg",
         web: "https://alapsraval.github.io/D3-data-journalism/",
         github: "https://github.com/alapsraval/D3-data-journalism/"
     },
