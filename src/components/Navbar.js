8c40fe8c3c34ba5840072ac46e92f91ea422f315
Alap Raval <alapsraval@gmail.com>
2021-09-05 11:17:49 -0500

    adding alt for accessibility

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ function Header() {
     return (
         <nav id="navbar" className="navbar nav-menu">
             <ul>
-                <li><a href="#about" className="nav-link scrollto"><FaUser size="1.5em" /> <span>About Me</span></a></li>
-                <li><a href="#portfolio" className="nav-link scrollto"><FaGithub size="1.5em" /> <span>Projects</span></a>
+                <li><a href="#about" className="nav-link scrollto"><FaUser size="1.5em" alt="about me" /> <span>About Me</span></a></li>
+                <li><a href="#portfolio" className="nav-link scrollto"><FaGithub size="1.5em" alt="portfolio" /> <span>Projects</span></a>
                 </li>
-                <li><a href="#experience" className="nav-link scrollto"><FaFileCode size="1.5em" /> <span>Experience</span></a>
+                <li><a href="#experience" className="nav-link scrollto"><FaFileCode size="1.5em" alt="experience" /> <span>Experience</span></a>
                 </li>
-                <li><a href="#contact" className="nav-link scrollto"><FaEnvelope size="1.5em" /> <span>Contact</span></a></li>
+                <li><a href="#contact" className="nav-link scrollto"><FaEnvelope size="1.5em" alt="contact" /> <span>Contact</span></a></li>
             </ul>
         </nav>
     );
